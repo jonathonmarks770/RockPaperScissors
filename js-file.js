98495f0bff124bea5f8d400debef0400a87c91a4
Jonathon Marks <jonathonmarks770@gmail.com>
2024-06-25 14:50:23 -0500

    Finalize functions

--- js-file.js
+++ js-file.js
@@ -1,12 +1,14 @@
 const buttons = document.querySelectorAll("button");
-
+const result = document.getElementById("result");
 let humanScore=0;
 let computerScore=0;
 
 buttons.forEach(button => {
     button.addEventListener("click", ()=>{
-    let result = playRound(button.id, getComputerChoice());
-    console.log(result);
+        if (humanScore == 5 || computerScore == 5) {
+            return false;
+        }playRound(button.id, getComputerChoice());
+        checkWinner();
     });
 });
 
@@ -18,26 +20,39 @@ function getComputerChoice(){
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === "rock" && computerChoice === "scissors") {
             humanScore++;
-            return `You win! Rock beats scissors! Score: ${humanScore} to ${computerScore}`;
+            return result.textContent = `You win! Rock beats scissors! Score: ${humanScore} to ${computerScore}`;
           } else if (humanChoice === "scissors" && computerChoice === "paper") {
             humanScore++;
-            return `You win! Scissors beats paper! Score: ${humanScore} to ${computerScore}`;
+            return result.textContent = `You win! Scissors beats paper! Score: ${humanScore} to ${computerScore}`;
           } else if (humanChoice === "paper" && computerChoice === "rock") {
             humanScore++;
-            return `You win! Paper beats rock! Score: ${humanScore} to ${computerScore}`;
+            return result.textContent = `You win! Paper beats rock! Score: ${humanScore} to ${computerScore}`;
           } else if (humanChoice ==="rock" && computerChoice ==="paper") {
             computerScore++;
-            return `You lose! Paper beats rock! Score: ${humanScore} to ${computerScore}`;
+            return result.textContent = `You lose! Paper beats rock! Score: ${humanScore} to ${computerScore}`;
           } else if (humanChoice === "paper" && computerChoice === "scissors") {
             computerScore++;
-            return `You lose! Scissors beats paper! Score: ${humanScore} to ${computerScore}`;
+            return result.textContent = `You lose! Scissors beats paper! Score: ${humanScore} to ${computerScore}`;
           } else if (humanChoice === "scissors" && computerChoice === "rock") {
             computerScore++;
-            return `You lose! rock beats scissors! Score: ${humanScore} to ${computerScore}`;
+            return result.textContent = `You lose! rock beats scissors! Score: ${humanScore} to ${computerScore}`;
           } else {
-            return `It\'s a tie! Score: ${humanScore} to ${computerScore}`;
+            return result.textContent = `It\'s a tie! Score: ${humanScore} to ${computerScore}`;
           }
+
 }
+function checkWinner(){
+    let winner = document.getElementById("winner");
+if (humanScore==5){
+    return winner.textContent="Congrats! You beat the computer";
+} else if(computerScore==5){
+   return winner.textContent="Game Over!";
+} 
+}
+
+
+
+
 
 
 
